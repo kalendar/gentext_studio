cd72733c80339e0b56af22868ca1a87f8a6e711b
JoshuaMaddy <110144926+JoshuaMaddy@users.noreply.github.com>
2025-04-01 17:15:41 -0400

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -11,8 +11,8 @@ if [ ! -f ".git/hooks/pre-commit" ]; then
     pre-commit install
 fi
 
-# Start tailwindcss in background
-npx tailwindcss -i ./src/treebeard/static/css/src/input.css -o ./src/treebeard/static/css/main.css -w &
+# Start tailwindcss, can't use same terminal because fastapi dev kills it on reload.
+npx tailwindcss -i ./src/treebeard/static/css/src/input.css -o ./src/treebeard/static/css/main.css -w
 
 # Start FastAPI server
 fastapi dev ./src/treebeard --port 5080
