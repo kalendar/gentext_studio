b05b59de0d6ee41576ee5f0fb4fecadbf4caf6ab
David Wiley <david.wiley@gmail.com>
2025-04-04 14:36:06 -0400

    Updates to dev.sh for Mac

--- dev.sh
+++ dev.sh
@@ -11,19 +11,8 @@ if [ ! -f ".git/hooks/pre-commit" ]; then
     pre-commit install
 fi
 
-# Start Tailwind CSS in the background
-npm ci
-npx tailwindcss -i ./src/treebeard/static/css/src/input.css -o ./src/treebeard/static/css/main.css -w &
-TAILWIND_PID=$!
-
-# Cleanup function
-cleanup() {
-    echo "Stopping Tailwind CSS watcher..."
-    kill $TAILWIND_PID
-}
-
-# Set up trap to clean up on script exit
-trap cleanup EXIT
+# Start tailwindcss in a new terminal window
+osascript -e 'tell app "Terminal" to do script "cd \"'$(pwd)'\" && npm ci && npx tailwindcss -i ./src/treebeard/static/css/src/input.css -o ./src/treebeard/static/css/main.css -w"' -e 'tell app "Terminal" to activate'
 
 # Start FastAPI server
-fastapi dev --port 5080
+fastapi dev ./src/treebeard --port 5080
